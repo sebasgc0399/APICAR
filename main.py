dd3f3feee7b1142d85004d4fc4b97a8d612d9e7f
Camilo Ramirez <camilos1900@gmail.com>
2023-11-20 18:42:14 -0500

    Inicio API

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+import json
+import os
+
+app = FastAPI()
+
+# Ruta de la carpeta de imágenes
+IMAGENES_DIR = "imagenes"
+
+# Cargar los datos de los vehículos desde el archivo
+def cargar_vehiculos():
+    try:
+        with open("./vehiculos/vehiculos.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+vehiculos = cargar_vehiculos()
+
+# Función para obtener la URL de la imagen del vehículo
+def obtener_url_imagen(id_vehiculo):
+    ruta_imagen = os.path.join(IMAGENES_DIR, f"{id_vehiculo}.jpg")
+    return ruta_imagen if os.path.exists(ruta_imagen) else None
+
+@app.get("/vehiculos")
+async def leer_vehiculos():
+    for vehiculo in vehiculos:
+        vehiculo["imagen_url"] = obtener_url_imagen(vehiculo["id"])
+    return vehiculos
+
+@app.get("/vehiculo/{id}")
+async def leer_vehiculo(id: int):
+    for vehiculo in vehiculos:
+        if vehiculo["id"] == id:
+            vehiculo["imagen_url"] = obtener_url_imagen(id)
+            return vehiculo
+    raise HTTPException(status_code=404, detail="Vehículo no encontrado")
+
+@app.get("/imagen_vehiculo/{id}")
+async def obtener_imagen_vehiculo(id: int):
+    ruta_imagen = obtener_url_imagen(id)
+    if ruta_imagen:
+        return FileResponse(ruta_imagen)
+    else:
+        raise HTTPException(status_code=404, detail="Imagen no encontrada")
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Ejecutar backend:
+
+Iniciar el entorno virtual:
+
+```python env/Scripts/{Seleccionar script indicado para su tipo de ejecucion}```
+
+Ejecutar el servidor de fastapi:
+
+```uvicorn main:app --reload```
