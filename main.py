33425a18fcb28d535cb0f3936f2a68b2d33dd1fa
Sebastian <sebasgc0399@gmail.com>
2023-11-20 21:28:24 -0500

    cinco

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 import json
- 
+import os
+
 app = FastAPI()
- 
+
+# Configuración para servir archivos estáticos desde la carpeta 'imagenes'
+app.mount("/imagenes", StaticFiles(directory="imagenes"), name="imagenes")
+
 # Cargar los datos de los vehículos desde el archivo
 def cargar_vehiculos():
     try:
-        with open("./vehiculos/vehiculos.json", "r") as file:
+        with open(os.path.join("vehiculos", "vehiculos.json"), "r") as file:
             return json.load(file)
     except FileNotFoundError:
-        return []
- 
+        raise HTTPException(status_code=404, detail="Archivo de vehículos no encontrado")
+
 vehiculos = cargar_vehiculos()
- 
+
+def obtener_ruta_imagen(id_vehiculo):
+    return os.path.join('imagenes', f"{id_vehiculo}.png")
+
 def obtener_url_imagen_d(id_vehiculo):
-    # Aquí generas la URL completa de la imagen
-    return f"https://apicarroslibre.azurewebsites.net/imagen_vehiculo/{id_vehiculo}"
- 
-# Función para obtener la URL de la imagen del vehículo
-def obtener_url_imagen(id_vehiculo):
-    ruta_imagen = './imagenes/' + str(id_vehiculo) + '.png'
-    return ruta_imagen if ruta_imagen else None
- 
+    return f"https://apicarroslibre.azurewebsites.net/imagenes/{id_vehiculo}.png"
+
 @app.get("/vehiculos")
 async def leer_vehiculos():
     for vehiculo in vehiculos:
-        print(vehiculo)
         vehiculo["imagen_url"] = obtener_url_imagen_d(vehiculo["id"])
     return vehiculos
- 
+
 @app.get("/vehiculo/{id}")
 async def leer_vehiculo(id: int):
-    for vehiculo in vehiculos:
-        if vehiculo["id"] == id:
-            vehiculo["imagen_url"] = obtener_url_imagen_d(id)
-            return vehiculo
+    vehiculo = next((v for v in vehiculos if v["id"] == id), None)
+    if vehiculo:
+        vehiculo["imagen_url"] = obtener_url_imagen_d(id)
+        return vehiculo
     raise HTTPException(status_code=404, detail="Vehículo no encontrado")
- 
-@app.get("/imagen_vehiculo/{id}")
-async def obtener_imagen_vehiculo(id: int):
-    ruta_imagen = obtener_url_imagen(id)
-    if ruta_imagen:
+
+@app.get("/imagenes/{id_vehiculo}.png")
+async def obtener_imagen_vehiculo(id_vehiculo: int):
+    ruta_imagen = obtener_ruta_imagen(id_vehiculo)
+    if os.path.isfile(ruta_imagen):
         return FileResponse(ruta_imagen)
     else:
         raise HTTPException(status_code=404, detail="Imagen no encontrada")
- 
